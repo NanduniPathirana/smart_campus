5526652e3e5dc558e7193998218b52b7eb475bdf
Nanduni Pathirana <nandunipathirana2003@gmail.com>
2026-04-18 15:53:25 +0530

    Project Setup - Configured pom.xml with Jersey, Grizzly, Jackson dependencies

---
+++ smart_campus/src/main/java/com/smartcampus/smart_campus/Smart_campus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.smartcampus.smart_campus;
+
+/**
+ *
+ * @author USER
+ */
+public class Smart_campus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
